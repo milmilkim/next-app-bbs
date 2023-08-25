7da3136a8b9fab6cb9d53d06d69a22142c1b931c
milmilkim <lukeplea00@lgupluspartners.co.kr>
2023-08-25 16:28:59 +0900

    key 개행

--- service_account.js
+++ service_account.js
@@ -1,8 +1,11 @@
+const {private_key} = JSON.parse(process.env.PRIVATE_KEY);
+
+
 const account =   {
   type: 'service_account',
   project_id: process.env.PROJECT_ID,
   private_key_id: process.env.PRIVATE_KEY_ID,
-  private_key: process.env.PRIVATE_KEY,
+  private_key,
   client_email: process.env.CLIENT_EMAIL,
   client_id: process.env.CLIENT_ID,
   auth_uri: process.env.AUTH_URI,
